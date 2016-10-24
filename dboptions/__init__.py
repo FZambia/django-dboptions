07fd99ae59210d0658686a40bdfb88250b643566
Alexandr Emelin <frvzmb@gmail.com>
2016-10-24 12:48:01 +0300

    0.2.0

--- dboptions/__init__.py
+++ dboptions/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from dboptions.models import Option
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = 'Alexandr Emelin'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015 Alexandr Emelin'
+__copyright__ = 'Copyright 2016 Alexandr Emelin'
